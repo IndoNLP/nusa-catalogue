64df648ec89d3865741c6859db4d342e95152929
Sonny Lazuardi <sonnylazuardi@gmail.com>
2024-02-14 14:30:37 +0800

    Add initial nextjs website

--- icons.js
+++
@@ -1,8 +0,0 @@
-icons =
-{"hf": '<span class="badge bg-primary">Open in 🤗</span> ',
-  "github": '<span class="badge bg-primary">Open in <i class="fab fa-github"> </i> </span> ',
-  "download": '<span class="badge bg-primary">Download link⬇</span> ',
-// "kaggle":'<i class="fa-brands fa-kaggle"></i>',
-// "gitlab":'<i class="fa-brands fa-gitlab"></i>',
-// "dropbox":'<i class="fa-brands fa-dropbox"></i>'
-}
