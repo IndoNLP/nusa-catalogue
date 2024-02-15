f1cfd9847637ab676baaf613fdc7f8806b548ff8
Sonny Lazuardi <sonnylazuardi@gmail.com>
2024-02-15 16:19:10 +0800

    Update cards

--- components/Dataset.tsx
+++ components/Dataset.tsx
@@ -10,21 +10,43 @@ import { Search } from "./ui/search";
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
 } from "./ui/pagination";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { LoadingSpinner } from "./ui/spinner";
 import { Button } from "./ui/button";
-import { Database } from "lucide-react";
+import {
+  Bot,
+  Database,
+  Eye,
+  FileType,
+  FolderCheck,
+  Globe,
+  ScrollText,
+  Speech,
+  Type,
+  Video,
+} from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 interface DatasetProps {
   name: string;
   description: string;
   datasetLink: string;
+  paperTitle: string;
+  paperLink: string;
+  language: string;
+  modality: string;
+  hfLink: string;
+  tasks: string;
 }
 
 const PER_PAGE = 15;
@@ -47,6 +69,12 @@ export const Dataset = () => {
         name: item["Dataset name"],
         description: item["Dataset description"],
         datasetLink: item["Dataset URL"],
+        paperTitle: item["Dataset paper title"],
+        paperLink: item["Dataset paper URL"],
+        language: item["Dataset language(s)"],
+        modality: item["Dataset modality"],
+        hfLink: item["HuggingFace URL"],
+        tasks: item["Dataset task(s)"],
       }));
       setData(dataset);
       setLoading(false);
@@ -93,8 +121,12 @@ export const Dataset = () => {
 
     for (let p = startPage; p <= endPage; p++) {
       pageNumbers.push(
-        <PaginationItem key={p}>
-          <PaginationLink isActive={p === page} onClick={() => setPage(p)}>
+        <PaginationItem key={p} role="button">
+          <PaginationLink
+            isActive={p === page}
+            onClick={() => setPage(p)}
+            href="#dataset"
+          >
             {p}
           </PaginationLink>
         </PaginationItem>
@@ -103,69 +135,170 @@ export const Dataset = () => {
 
     return [
       <PaginationPrevious
+        role="button"
         key="prev"
+        href="#dataset"
         onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
       />,
       ...pageNumbers,
       <PaginationNext
+        role="button"
         key="next"
+        href="#dataset"
         onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
       />,
     ];
   };
 
+  const formatNewline = (text: string) => {
+    return (text || "").split("\r\n").join(", ").split("\n").join(", ");
+  };
+
   return (
-    <section id="dataset" className="container py-24 sm:py-32 !pt-0">
-      <div className="flex flex-row justify-end mb-8">
-        <Search
-          placeholder="Search Dataset..."
-          className="w-full max-w-96"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
-
-      {loading ? (
-        <div className="h-40 flex justify-center items-center">
-          <LoadingSpinner />
+    <TooltipProvider>
+      <section id="dataset" className="container py-24 sm:py-32 !pt-20">
+        <div className="flex flex-row mb-8">
+          <div className="text-neutral-400">
+            Showing {filteredData.length} dataset
+          </div>
+          <div className="flex flex-1"></div>
+          <Search
+            placeholder="Search Dataset..."
+            className="w-full max-w-96"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
-      ) : null}
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2  lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6 mb-8">
-        {filterPage().map(
-          ({ name, description, datasetLink }: DatasetProps) => (
-            <Card
-              key={name}
-              className="max-w-md md:break-inside-avoid overflow-hidden"
-            >
-              <CardHeader className="flex flex-row items-center gap-4 pb-2">
-                <div className="flex flex-col">
-                  <CardTitle className="text-lg">{name}</CardTitle>
-                  {/* <CardDescription>{userName}</CardDescription> */}
-                </div>
-              </CardHeader>
+        {loading ? (
+          <div className="h-40 flex justify-center items-center">
+            <LoadingSpinner />
+          </div>
+        ) : null}
 
-              <CardContent>
-                <div className="line-clamp-3">{description}</div>
-              </CardContent>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2  lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6 mb-8">
+          {filterPage().map((item: DatasetProps) => {
+            const {
+              name,
+              description,
+              datasetLink,
+              paperLink,
+              paperTitle,
+              language,
+              modality,
+              hfLink,
+              tasks,
+            } = item;
 
-              <CardFooter className="flex justify-end">
-                <a href={datasetLink}>
-                  <Button>
-                    <Database className="mr-2" /> Dataset
-                  </Button>
-                </a>
-              </CardFooter>
-            </Card>
-          )
-        )}
-      </div>
+            const renderModalityIcon = (modality: string) => {
+              switch (modality) {
+                case "Language":
+                  return <FileType size={20} />;
+                case "Vision":
+                  return <Eye size={20} />;
+                case "Speech":
+                  return <Speech size={20} />;
+                case "Video":
+                  return <Video size={20} />;
+              }
+            };
 
-      <div>
-        <Pagination>
-          <PaginationContent>{renderPagination()}</PaginationContent>
-        </Pagination>
-      </div>
-    </section>
+            const renderModality = () => {
+              return (
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <div className="flex flex-row space-x-1">
+                      {modality
+                        .split("\n")
+                        .map((modality) => renderModalityIcon(modality))}
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    {modality.split("\n").join(", ")}
+                  </TooltipContent>
+                </Tooltip>
+              );
+            };
+            return (
+              <Card
+                key={name}
+                className="max-w-md md:break-inside-avoid overflow-hidden"
+              >
+                <CardHeader className="flex flex-row items-center gap-4 pb-2">
+                  <div className="flex flex-col">
+                    <a href={datasetLink}>
+                      <CardTitle className="text-lg">{name}</CardTitle>
+                    </a>
+                  </div>
+                </CardHeader>
+
+                <CardContent>
+                  <div className="line-clamp-3">{description}</div>
+                  {language ? (
+                    <div className="flex flex-row items-center text-neutral-400 text-sm mt-2">
+                      <div className="w-5 mr-2">
+                        <Globe size={20} />
+                      </div>
+                      <div className="line-clamp-2">
+                        {formatNewline(language)}
+                      </div>
+                    </div>
+                  ) : null}
+
+                  {tasks ? (
+                    <div className="flex flex-row items-center text-neutral-400 text-sm mt-2">
+                      <div className="w-5 mr-2">
+                        <FolderCheck size={20} />
+                      </div>
+                      <div className="line-clamp-2">{formatNewline(tasks)}</div>
+                    </div>
+                  ) : null}
+                </CardContent>
+
+                <CardFooter className="flex space-x-2 mt-4">
+                  {renderModality()}
+
+                  <div className="flex flex-1"></div>
+                  {hfLink ? (
+                    <a href={hfLink}>
+                      <Button variant="outline">
+                        <Bot size={20} className="mr-2" /> HF
+                      </Button>
+                    </a>
+                  ) : null}
+
+                  {paperLink ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <a href={paperLink}>
+                          <Button variant="outline">
+                            <ScrollText size={20} className="mr-2" /> Paper
+                          </Button>
+                        </a>
+                      </TooltipTrigger>
+                      <TooltipContent>{paperTitle}</TooltipContent>
+                    </Tooltip>
+                  ) : null}
+
+                  {datasetLink ? (
+                    <a href={datasetLink}>
+                      <Button>
+                        <Database size={20} className="mr-2" /> Data
+                      </Button>
+                    </a>
+                  ) : null}
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </div>
+
+        <div>
+          <Pagination>
+            <PaginationContent>{renderPagination()}</PaginationContent>
+          </Pagination>
+        </div>
+      </section>
+    </TooltipProvider>
   );
 };
