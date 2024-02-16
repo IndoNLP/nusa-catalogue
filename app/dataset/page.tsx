c7c0b2e823d5d4e08fcc42ca945b975ff1a1ae0f
Sonny Lazuardi <sonnylazuardi@gmail.com>
2024-02-16 23:56:41 +0800

    Add detail page

--- app/dataset/page.tsx
+++ app/dataset/page.tsx
@@ -5,19 +5,28 @@ import { Navbar } from "@/components/Navbar";
 import { useDatasetFetch } from "@/components/helper/hooks";
 import { LoadingSpinner } from "@/components/ui/spinner";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Page() {
+const DatasetCard = () => {
   const searchParams = useSearchParams();
   const { data } = useDatasetFetch();
   const item = data.find((d) => d.id === searchParams.get("id"));
   return (
+    <section className="container py-24 sm:py-32 !pt-20">
+      <div className="w-full max-w-[800px] mx-auto">
+        {item ? <Card item={item} showDetail /> : <LoadingSpinner />}
+      </div>
+    </section>
+  );
+};
+
+export default function Page() {
+  return (
     <>
       <Navbar />
-      <section className="container py-24 sm:py-32 !pt-20">
-        <div className="w-full max-w-[800px] mx-auto">
-          {item ? <Card item={item} showDetail /> : <LoadingSpinner />}
-        </div>
-      </section>
+      <Suspense fallback={<LoadingSpinner />}>
+        <DatasetCard />
+      </Suspense>
     </>
   );
 }
